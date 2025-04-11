ffd61602f5508652d3798b3323754474c5927673
Idan Schiller <idan@Idans-MacBook-Air.local>
2025-04-11 22:55:19 +0300

    Add Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
